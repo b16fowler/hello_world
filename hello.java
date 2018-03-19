80d065f465e6cea7e3923b4fcef8ca39c0daf874
b16fowler <b16fowler@gmail.com>
2018-03-19 09:49:31 -0400

    Update hello.java
    Takes user input and prints names

--- hello.java
+++ hello.java
@@ -1,15 +1,18 @@
 // a simple program to practice common Github commands 
+import java.util.Scanner
 
 public class hello {
+
+    Scanner scnr = new Scanner(System.in);
 
     public static void main(String [] args) {
 
 
 	
-	// added  line	
-	// added another line
+	System.out.println("Enter your name: ");
+    String name = scnr.next();
 
-        System.out.println("Hello Class!");
+    System.out.println("Hello, " + name + " my name is Brett");
 
 
 
